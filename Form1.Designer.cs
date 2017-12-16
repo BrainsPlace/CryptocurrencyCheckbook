bdd5344135ea6c2b749e586939939c239c38e32a
BrainsPlace <brianmcclaugherty@gmail.com>
2017-12-16 17:36:19 -0500

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Cryptocurrency_Checkbook
+{
+    partial class CryptoCheckbook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Currency = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Quantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PurchasePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PaidPerCoin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NowPerCoin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Net = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Link = new System.Windows.Forms.DataGridViewLinkColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Currency,
+            this.Quantity,
+            this.PurchasePrice,
+            this.PaidPerCoin,
+            this.NowPerCoin,
+            this.Net,
+            this.Link});
+            this.dataGridView.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(763, 245);
+            this.dataGridView.TabIndex = 0;
+            this.dataGridView.CellValidated += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellValidated);
+            // 
+            // Currency
+            // 
+            this.Currency.HeaderText = "Currency";
+            this.Currency.Name = "Currency";
+            // 
+            // Quantity
+            // 
+            this.Quantity.HeaderText = "Quantity";
+            this.Quantity.Name = "Quantity";
+            // 
+            // PurchasePrice
+            // 
+            this.PurchasePrice.HeaderText = "Purchase Price (USD)";
+            this.PurchasePrice.Name = "PurchasePrice";
+            // 
+            // PaidPerCoin
+            // 
+            this.PaidPerCoin.HeaderText = "$ / Coin (Paid)";
+            this.PaidPerCoin.Name = "PaidPerCoin";
+            // 
+            // NowPerCoin
+            // 
+            this.NowPerCoin.HeaderText = "$ / Coin (Now)";
+            this.NowPerCoin.Name = "NowPerCoin";
+            // 
+            // Net
+            // 
+            this.Net.HeaderText = "Net Gain (USD)";
+            this.Net.Name = "Net";
+            // 
+            // Link
+            // 
+            this.Link.HeaderText = "Link";
+            this.Link.Name = "Link";
+            this.Link.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(94, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CryptoCheckbook
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 472);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "CryptoCheckbook";
+            this.Text = "Cryptocurrency Checkbook";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Currency;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Quantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PurchasePrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PaidPerCoin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NowPerCoin;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Net;
+        private System.Windows.Forms.DataGridViewLinkColumn Link;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cryptocurrency_Checkbook
+{
+    public partial class CryptoCheckbook : Form
+    {
+        public CryptoCheckbook()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView_CellValidated(object sender, DataGridViewCellEventArgs e)
+        {
+            UpdateMarketValues();
+            UpdatePerCoinPaid();
+            UpdateNetGain();
+        }            
+
+        private void UpdateMarketValues()
+        {
+            foreach(DataGridViewRow r in dataGridView.Rows)
+            {
+                if(r.Cells["Currency"].Value != null && r.Cells["Currency"].Value.ToString() != "")
+                {
+                    string json = GetMarketValue(r.Cells["Currency"].Value.ToString());
+                    json = json.Replace("[", "").Replace("]",""); //only ever going to be 1 object
+                    var obj = JsonConvert.DeserializeObject<Currency>(json);
+                    r.Cells["NowPerCoin"].Value = obj.price_usd;
+
+                }
+            }
+        }
+
+        private void UpdateNetGain()
+        {
+            foreach (DataGridViewRow r in dataGridView.Rows)
+            {
+                try
+                {
+                    if ((r.Cells["PaidPerCoin"].Value != null && r.Cells["PaidPerCoin"].Value.ToString() != "")
+                        && (r.Cells["NowPerCoin"].Value != null && r.Cells["NowPerCoin"].Value.ToString() != "")
+                        && (r.Cells["Quantity"].Value != null && r.Cells["Quantity"].Value.ToString() != ""))
+                    {
+                        decimal paidValue = Convert.ToDecimal(r.Cells["PaidPerCoin"].Value);
+                        decimal quantity = Convert.ToDecimal(r.Cells["Quantity"].Value);
+                        decimal marketValue = Convert.ToDecimal(r.Cells["NowPerCoin"].Value);
+
+
+                        r.Cells["Net"].Value = Convert.ToString((marketValue - paidValue) * quantity);
+                        if (Convert.ToDecimal(r.Cells["Net"].Value) < 0)
+                            r.Cells["Net"].Style.BackColor = Color.Red;
+                        else
+                            r.Cells["Net"].Style.BackColor = Color.Green;
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+
+        private void UpdatePerCoinPaid()
+        {
+            foreach (DataGridViewRow r in dataGridView.Rows)
+            {
+                try
+                {
+                    if ((r.Cells["PurchasePrice"].Value != null && r.Cells["PurchasePrice"].Value.ToString() != "")
+                        && (r.Cells["Quantity"].Value != null && r.Cells["Quantity"].Value.ToString() != ""))
+                    {
+                        decimal paidValue = Convert.ToDecimal(r.Cells["PurchasePrice"].Value);
+                        decimal quantity = Convert.ToDecimal(r.Cells["Quantity"].Value);
+
+                        r.Cells["PaidPerCoin"].Value = Convert.ToString(paidValue/quantity);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+        }
+
+        private string GetMarketValue(string currency)
+        {
+            string url = string.Format(@"https://api.coinmarketcap.com/v1/ticker/{0}/?convert=USD", currency);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                return "error";//throw;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveEntries();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView.Rows.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LoadEntries();
+        }
+
+        private void SaveEntries()
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(@"UserEntries.txt");
+            foreach (DataGridViewRow r in dataGridView.Rows)
+            {
+                if ((r.Cells["Currency"].Value != null && r.Cells["Currency"].Value.ToString() != "")
+                    && (r.Cells["PurchasePrice"].Value != null && r.Cells["PurchasePrice"].Value.ToString() != "")
+                    && (r.Cells["Quantity"].Value != null && r.Cells["Quantity"].Value.ToString() != ""))
+                {
+                    file.WriteLine(r.Cells["Currency"].Value + ";" + r.Cells["PurchasePrice"].Value + ";" + r.Cells["Quantity"].Value + ";");
+                }
+            }
+            file.Close();
+        }
+
+        private void LoadEntries()
+        {
+            dataGridView.Rows.Clear();
+
+            System.IO.StreamReader file = new System.IO.StreamReader(@"UserEntries.txt");
+            string line;
+            while ((line = file.ReadLine()) != null)
+            {
+                DataGridViewRow row = (DataGridViewRow)dataGridView.Rows[0].Clone();
+                int loc = line.IndexOf(";"); //first
+                row.Cells[0].Value = line.Substring(0, loc); //currency
+
+                line = line.Substring(loc + 1);
+                loc = line.IndexOf(";"); //second
+                row.Cells[2].Value = line.Substring(0, loc); //quantity
+
+                line = line.Substring(loc + 1);
+                loc = line.IndexOf(";"); //thirs
+                row.Cells[1].Value = line.Substring(0, loc); //price paid
+
+                dataGridView.Rows.Add(row);
+            }
+            UpdateMarketValues();
+            UpdatePerCoinPaid();
+            UpdateNetGain();
+        }
+        
+    }
+}
